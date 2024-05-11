9bbd7c243e4dc83d2b53374726fceab7e92fb5f9
jakubd255 <jakubdudek255@gmail.com>
2024-05-11 21:38:44 +0200

    Additional unit tests for JwtGenerator

--- src/test/java/com/example/springbootstarter/JwtGeneratorTest.java
+++ src/test/java/com/example/springbootstarter/JwtGeneratorTest.java
@@ -1,6 +1,8 @@
 package com.example.springbootstarter;
 
 import com.example.springbootstarter.jwt.JwtGenerator;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +10,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 public class JwtGeneratorTest {
     private JwtGenerator jwtGenerator;
+    private final String testEmail = "test@gmail.com";
 
     @BeforeEach
     public void setUp() {
@@ -21,12 +24,31 @@ public class JwtGeneratorTest {
 
     @Test
     public void testCreateTokenAndRead() {
-        String testEmail = "test@gmail.com";
         String token = jwtGenerator.generateToken(testEmail);
 
         String emailFromToken = jwtGenerator.extractUsername(token);
 
         Assertions.assertEquals(testEmail, emailFromToken);
         Assertions.assertTrue(jwtGenerator.isTokenValid(token, testEmail));
+    }
+
+    @Test
+    public void testMalformedToken() {
+        String token = "gdfgsfgshdgfjshgdfhjsgdjfhgsdjfgs";
+        Exception e = Assertions.assertThrows(
+                MalformedJwtException.class, () -> jwtGenerator.extractUsername(token)
+        );
+        System.out.println(e.getMessage());
+    }
+
+    @Test
+    public void testExpiredToken() {
+        ReflectionTestUtils.setField(jwtGenerator, "jwtExpirationTime", -1000);
+        String token = jwtGenerator.generateToken(testEmail);
+
+        Exception e = Assertions.assertThrows(
+                ExpiredJwtException.class, () -> jwtGenerator.extractUsername(token)
+        );
+        System.out.println(e.getMessage());
     }
 }
